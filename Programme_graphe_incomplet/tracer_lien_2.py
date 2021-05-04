b56dd612f699443129920b20e3259335b296b7dc
valmenucerri <valentin.menucerri@gmail.com>
2021-05-04 16:21:43 +0200

    méthode incomplète bien avancée

--- Programme_graphe_incomplet/tracer_lien_2.py
+++ Programme_graphe_incomplet/tracer_lien_2.py
@@ -1,7 +1,36 @@
-def petit_cout(liste,N):
+def petit_cout(liste):
     cout = []
     for i in liste:
         cout.append(i[1])
     petit = min(cout)
     petit_cout = cout.index(petit)
     return petit_cout
+
+def petit_noeud(L):
+    noeud = []
+    for i in L:
+        noeud.append(len(i))
+    petit = min(noeud)
+    petit_noeud = noeud.index(petit)
+    return petit_noeud
+
+def parcourir_graphe(L,N):
+    parcours = []
+    L_vide = [[] for i in range (N)]
+    indice = indice_prec = petit_noeud(L)
+    parcours.append('->'+ str(indice))
+    while L != L_vide:
+        noeud = L[indice]
+        if noeud == []:
+            indice = indice_prec
+        else:
+            faible_cout = petit_cout(noeud)
+            indice_prec = indice
+            indice = noeud[faible_cout][1]
+            parcours.append('-> '+ str(indice))
+            del noeud[faible_cout]
+
+
+    return parcours
+
+
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Executer le programme avec n le nombre de points voulu
 '''
 from Programme import trace_graphe as g, lier_points as lp
 import sys
-import Programme_graphe_incomplet.traiter_graphe as gr
+from Programme_graphe_incomplet import tracer_lien_2 as lp2,traiter_graphe as gr
 if '__main__' == __name__:
     '''n = int(sys.argv[-1])
     x,y = g.creer_pt(n)
@@ -13,4 +13,5 @@ if '__main__' == __name__:
     graphe = gr.recup_fichier(sys.argv)
     N = gr.nbr_noeud(graphe)
     L = gr.analyser_graphe(graphe,N)
-    print(L)
+    parcours = lp2.parcourir_graphe(L,N)
+    print(parcours)
