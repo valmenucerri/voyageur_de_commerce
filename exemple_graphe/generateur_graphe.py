3ef6dc18bb8879ee124b2a4bf7b1174fc6ac1b19
valmenucerri <valentin.menucerri@gmail.com>
2021-05-04 15:03:49 +0200

    première approche graphe incomplet

---
+++ exemple_graphe/generateur_graphe.py
@@ -0,0 +1,20 @@
+'''
+Créer un fichier .net qui contient des données aléatoires
+'''
+
+import random as r
+
+N = 10  # A changer comme désiré
+
+
+with open('exemple_graphe/exemple{}.net'.format(N), 'w') as t:
+    t.write(str(N) + '\n')
+    liste_ligne = []
+    for a in range(N - 1):
+        for b in range(int(r.randint(0, N - 1) ** 1 / 2)):
+            j = str(r.randint(a+1, N - 1))
+            h = str(r.randint(0, N - 1))
+            if [j, str(a)] not in liste_ligne:
+                t.write(str(a) + " " + j + " " + h +"\n")
+                t.write(j + " " + str(a) + "\n")
+                liste_ligne.append([j, str(a)])
