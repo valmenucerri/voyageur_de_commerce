482203953d25e777ceff2ced548672b5b3210d0c
valmenucerri <valentin.menucerri@gmail.com>
2021-05-04 17:17:07 +0200

    à corriger, le chemin semble incorrect

--- Programme_graphe_incomplet/tracer_lien_2.py
+++ Programme_graphe_incomplet/tracer_lien_2.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def petit_cout(liste):
     cout = []
     for i in liste:
@@ -6,29 +8,52 @@ def petit_cout(liste):
     petit_cout = cout.index(petit)
     return petit_cout
 
-def petit_noeud(L):
+def petit_noeud(L,N):
     noeud = []
-    for i in L:
-        noeud.append(len(i))
+    l_neuf = deepcopy(L)
+    print('l_neuf avant modif :',l_neuf)
+    for i,j in enumerate(l_neuf):
+        if j == []:
+            l_neuf[i] = [1 for _ in range(N)]
+            noeud.append(N)
+        else:
+            noeud.append(len(j))
     petit = min(noeud)
     petit_noeud = noeud.index(petit)
     return petit_noeud
 
 def parcourir_graphe(L,N):
     parcours = []
-    L_vide = [[] for i in range (N)]
-    indice = indice_prec = petit_noeud(L)
+    indice_prec =[]
+    indice  = petit_noeud(L,N)
+    indice_prec.append(indice)
     parcours.append('->'+ str(indice))
-    while L != L_vide:
+    nbr_noeud = 1
+    while nbr_noeud != N:
         noeud = L[indice]
         if noeud == []:
-            indice = indice_prec
+            indice = indice_prec[-retour]
+            retour += 1
+            if retour >= len(indice_prec):
+                indice = petit_noeud(L, N)
+
+
         else:
             faible_cout = petit_cout(noeud)
-            indice_prec = indice
-            indice = noeud[faible_cout][1]
-            parcours.append('-> '+ str(indice))
-            del noeud[faible_cout]
+            indice_prec.append(indice)
+            indice = noeud[faible_cout][0]
+            if str(indice) not in parcours:
+                nbr_noeud += 1
+                print(parcours)
+                parcours.append('-> ')
+                parcours.append(str(indice))
+
+            else:
+                parcours.append('<->')
+                parcours.append(str(indice))
+            del L[indice_prec[-1]][faible_cout]
+            retour = 1
+
 
 
     return parcours
