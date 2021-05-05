e828bb8a2873f106573dcecc211dd9eb54fb277e
valmenucerri <valentin.menucerri@gmail.com>
2021-05-05 15:19:34 +0200

    programmer terminé avec graphe tracé (pas magnifique)

--- Programme_graphe_incomplet/tracer_graphe_2.py
+++ Programme_graphe_incomplet/tracer_graphe_2.py
@@ -2,6 +2,8 @@ import Programme.lier_points as lp
 import matplotlib.pyplot as plt
 import random as r
 
+
+
 def trouver_milieu(pt1,pt2):
     x_mil = (pt1[0]+pt2[0]) / 2
     y_mil = (pt1[1]+pt2[1]) / 2
@@ -17,5 +19,11 @@ def tracer_graphe(L,N):
     for pt in range (N):
         plt.scatter(x[pt],y[pt],s=N*7,color = 'orange')
         plt.text(x[pt]-1,y[pt]-1,str(pt))
+    for i in range (N):
+        for j in L[i]:
+            lp.lier_points(coordonnees[i],coordonnees[j[0]])
+            milieu = trouver_milieu(coordonnees[i],coordonnees[j[0]])
 
-    plt.show()
+            plt.text(milieu[0],milieu[1],str(j[1]),color= 'green')
+    plt.rcParams['font.family'] = 'Arial Unicode MS'
+    plt.savefig("Graphe/exemple{}figure.pdf".format(len(coordonnees)))
